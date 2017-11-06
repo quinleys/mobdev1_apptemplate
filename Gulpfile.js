c2a3a606a115e25d86e5835fae14301d1b5f1c76
quinleys <quinleys@student.arteveldehs.be>
2017-11-06 15:04:15 +0100

    init

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,14 +5,27 @@ var gulpSass    = require('gulp-sass');
 //Static Server & waatch scss / html files
 gulp.task('serve',function(){
     browserSync.init({
-        server: './app'
+        server: './app',
+        port: 8080,
+        ui: {
+            port: 8081,
+            weinre:{
+                port: 9081
+            }
+        }
     });
+
     gulp.watch('app/scss/*.scss', ['sass']);
+    gulp.watch('app/*.html').on('change', browserSync.reload)
 });
 // Compile sass into CSS
 
 gulp.task('sass', function(){
     return gulp.src('app/scss/*.scss')
     .pipe(gulpSass())
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
 });
+
+//Default Task
+gulp.task('default',['serve']);
