4968202d25c62e843c62c438ca4ed0e08ca5ade0
quinleys <quinleys@student.arteveldehs.be>
2017-11-06 14:08:36 +0100

    init

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,3 +2,10 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var gulpSass    = require('gulp-sass');
 
+// Compile sass into CSS
+
+gulp.task('sass', function(){
+    return gulp.src('app/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'));
+});
